481c7979d04321be81980769c8e6f3b38eccd606
David Rodirguez <programacion@h2oins.com>
2026-03-25 22:45:56 -0500

    chore: update module exports with new components

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,5 +4,8 @@ H2O Quote RPA - Modules Package
 Core modules for automated quote processing.
 """
 
-__version__ = "0.1.0"
-__all__ = ["excel_config", "commodity_matcher", "pdf_extractor"]
+__version__ = "0.2.0"
+__all__ = [
+    "excel_config", "commodity_matcher", "pdf_extractor",
+    "quote_profile", "document_ai_extractor", "rule_engine",
+]
